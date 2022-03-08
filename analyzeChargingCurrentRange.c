1c3a748d799130e6599c1f3fde4b54037a403dd1
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-03-08 11:31:59 +0530

    Update analyzeChargingCurrentRange.c

--- analyzeChargingCurrentRange.c
+++ analyzeChargingCurrentRange.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "analyzeChargingCurrentRange.h"
 
+char* getChargingCurrentRangeAndSamples(int samplesOfChargingCurrent[]){
+	int lowerRangeLimit = 0, upperRangeLimit = 0, numSamples = 0;
+	char *chargingCurrentRangeAndSamples = (char*)malloc(100);
+	sprintf(chargingCurrentRangeAndSamples, "%d-%d, %lu, lowerRangeLimit, upperRangeLimit, numSamples);
+	return chargingCurrentRangeAndSamples;
+}
