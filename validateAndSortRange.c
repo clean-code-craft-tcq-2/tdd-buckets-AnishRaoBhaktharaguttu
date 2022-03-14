24b49925b34e9efc41dda3625608ed4bdfb3eeab
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-03-14 12:07:27 +0530

    Update validateAndSortRange.c

--- validateAndSortRange.c
+++ validateAndSortRange.c
@@ -20,6 +20,6 @@ int compare (const void * a, const void * b)
 }
 
 //Function to sort the given charging samples
-int* sortChargingSamples(int* chargingSamples, numSamples){
+int* sortChargingSamples(int* chargingSamples, int numSamples){
      return qsort (chargingSamples, numSamples, sizeof(int), compare);
 }
