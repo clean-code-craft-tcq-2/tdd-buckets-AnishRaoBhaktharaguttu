ec96a0b15cc1a83dfdc146dc3693861797f3dafa
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-03-14 15:50:08 +0530

    Update analyzeChargingCurrentRange.h

--- analyzeChargingCurrentRange.h
+++ analyzeChargingCurrentRange.h
@@ -1,4 +1,4 @@
 #include "validateAndSortRange.h"
 
 char* getChargingCurrentRangeAndSamples(int* samplesOfChargingCurrent);
-RangeStructure *buildRanges(int* sortedSamplesOfChargingCurrent, int numSamples)
+RangeStructure *buildRanges(int* sortedSamplesOfChargingCurrent, int numSamples);
