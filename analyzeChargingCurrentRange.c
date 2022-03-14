65c03ca47826eeb8db82044afcef193efb827dae
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-03-14 13:36:20 +0530

    Added build range function

--- analyzeChargingCurrentRange.c
+++ analyzeChargingCurrentRange.c
@@ -17,7 +17,35 @@ char* getChargingCurrentRangeAndSamples(int samplesOfChargingCurrent[]){
 	         int sortedSamplesOfChargingCurrent[numSamples] = sortChargingSamples(int* samplesOfChargingCurrent, int numSamples);
 		for (int n=0; n<numSamples; n++)
 			printf("%d\n", sortedSamplesOfChargingCurrent);
+		
+		struct RangeStructure* rangeStructureArray = sortedSamplesOfChargingCurrent(samplesOfChargingCurrent, numSamples);
 	}
 	return chargingCurrentRangeAndSamples;
 }
 
+Struct RangeStructure* buildRanges(int sortedSamplesOfChargingCurrent[], int numSamples) {
+    struct RangeStructure *rangeStructureArray = malloc(sizeof(struct RangeStructure) * 20);
+    
+    int rangeStart = sortedSamplesOfChargingCurrent[0];
+    int lastValue = sortedSamplesOfChargingCurrent[0];
+    int occurences = 1;
+    for (int i = 1; i < numSamples; i++) {
+        int value = sortedSamplesOfChargingCurrent[i];
+        if (value != lastValue + 1) {
+            // When it enters this condition, the range has ended
+	    rangeStructureArray[i].LowerLimit = rangeStart;
+	    rangeStructureArray[i].UpperLimit = lastValue;
+	    rangeStructureArray[i].Occurences = occurences;
+	    rangeStart = value;
+	    occurences = 1;
+	    lastValue = value;
+	    if(i==numSamples-1){
+	       // Conclude last range
+    		rangeStructureArray[i+1].LowerLimit = rangeStart;
+    		rangeStructureArray[i+1].UpperLimit = lastValue;
+    		rangeStructureArray[i+1].Occurences = occurences;  
+        }
+	occurences++;
+    }
+    return rangeStructureArray;
+}
