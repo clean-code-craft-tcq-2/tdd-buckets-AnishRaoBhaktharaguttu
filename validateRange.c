5f3288a955e54074e68221d93110593c8caeba3b
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-03-14 11:49:12 +0530

    Update validateRange.c

--- validateRange.c
+++ validateRange.c
@@ -1,3 +1,5 @@
+#include "validateRange.h"
+
 // Function to validate if the charging samples are non empty non negative
 int negativeNumberInArray(int *chargingSamples, int numSamples){
     if(numSamples > 0){
