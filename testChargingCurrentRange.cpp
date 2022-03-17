2e77a2a95f1fde6abf518c7b02a1f8311d3b0e21
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-03-17 13:18:20 +0530

    Update testChargingCurrentRange.cpp

--- testChargingCurrentRange.cpp
+++ testChargingCurrentRange.cpp
@@ -7,5 +7,9 @@ TEST_CASE("Checks the range of the charging current and the number of samples wi
 	int samplesOfChargingCurrent[] = {1,3,5,2,7,9,4};
 	const char* expectedRangeAndSamples = "1-5, 5, 7-7, 1, 9-9, 1";
 	char* chargingCurrentRangeAndSamples = getChargingCurrentRangeAndSamples(samplesOfChargingCurrent, 7);
+	for(int i =0; i<3; i++){
+	printf("Range, Readings \n");
+	printf("%s\n", chargingCurrentRangeAndSamples);
+	}
 	REQUIRE(strcmp(chargingCurrentRangeAndSamples, expectedRangeAndSamples) == 0);
 }
