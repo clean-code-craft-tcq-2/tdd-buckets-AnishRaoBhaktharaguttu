dd52910b9011be3d0264ff2f5ecbf41b3e9495b5
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-03-17 14:36:31 +0530

    Update testChargingCurrentRange.cpp

--- testChargingCurrentRange.cpp
+++ testChargingCurrentRange.cpp
@@ -6,13 +6,20 @@
 TEST_CASE("Checks the range of the charging current and the number of samples within the range") {
 	int samplesOfChargingCurrent[] = {1,3,5,2,7,9,4};
 	char* rangeAndOccurences[3];
-	const char* expectedRangeAndSamples = "1-5, 5, 7-7, 1, 9-9, 1";
 	getChargingCurrentRangeAndSamples(samplesOfChargingCurrent, 7, rangeAndOccurences);
-	printf("Range, Readings \n");
-	for(int i =0; i<3; i++){
-	printf("%s\n", rangeAndOccurences[i]);
-	}
 	REQUIRE(strcmp(rangeAndOccurences[0], "1-5, 5") == 0);
 	REQUIRE(strcmp(rangeAndOccurences[1], "7-7, 1") == 0);
 	REQUIRE(strcmp(rangeAndOccurences[2], "9-9, 1") == 0);
+}
+
+TEST_CASE("Checks invalid use case -  Negative values") {
+	int samplesOfChargingCurrent[] = {1,3,-5,2,7,9,4};
+	char* rangeAndOccurences[3];
+	getChargingCurrentRangeAndSamples(samplesOfChargingCurrent, 7, rangeAndOccurences);
+}
+
+TEST_CASE("Checks invalid use case - No values") {
+	int samplesOfChargingCurrent[] = {};
+	char* rangeAndOccurences[3];
+	getChargingCurrentRangeAndSamples(samplesOfChargingCurrent, 7, rangeAndOccurences);
 }
