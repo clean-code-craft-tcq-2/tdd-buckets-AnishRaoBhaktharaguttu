dab38231dc4adfe3ead5d282fcc844058f31704d
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-03-17 14:23:35 +0530

    Update analyzeChargingCurrentRange.c

--- analyzeChargingCurrentRange.c
+++ analyzeChargingCurrentRange.c
@@ -23,9 +23,12 @@ void getChargingCurrentRangeAndSamples(int* samplesOfChargingCurrent, int numSam
 		buildRanges(sortedSamplesOfChargingCurrent, numSamples, rangeStructureArray);
 	        size_t sizeOfRangeStructureArray = (sizeof(rangeStructureArray) / sizeof(rangeStructureArray[0]));
 		printf("Size of range struct array is %lu ", sizeOfRangeStructureArray);
+		for (int i=0; i < (int)sizeOfRangeStructureArray; i++) {
+		rangeAndOccurences[i] = (char*)malloc(10);
+	        }
 		for(int i =0; i<(int)sizeOfRangeStructureArray; i++){
 			printf("DANGER");
-			//sprintf(rangeAndOccurences[i], "%d-%d, %d", rangeStructureArray[i].LowerLimit, rangeStructureArray[i].UpperLimit, rangeStructureArray[i].Occurences);
+			sprintf(rangeAndOccurences[i], "%d-%d, %d", rangeStructureArray[i].LowerLimit, rangeStructureArray[i].UpperLimit, rangeStructureArray[i].Occurences);
 			printf("Range, Readings \n");
 			printf("%s\n", rangeAndOccurences[i]);
 		}
