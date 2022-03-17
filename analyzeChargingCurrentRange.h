516b293a71566447ae72782c027ae39853e151e5
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-03-17 14:32:16 +0530

    Update analyzeChargingCurrentRange.h

--- analyzeChargingCurrentRange.h
+++ analyzeChargingCurrentRange.h
@@ -2,3 +2,4 @@
 
 void getChargingCurrentRangeAndSamples(int* samplesOfChargingCurrent, int numSamples, char* rangeAndOccurences[]);
 void buildRanges(int* sortedSamplesOfChargingCurrent, int numSamples, RangeStructure* rangeStructureArray);
+void printOutputinCsv(size_t sizeOfRangeStructureArray, RangeStructure rangeStructureArray[], char* rangeAndOccurences[]);
