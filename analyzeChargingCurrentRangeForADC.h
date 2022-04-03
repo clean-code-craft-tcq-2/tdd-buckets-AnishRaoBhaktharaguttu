35fafbaf412d8a1412c36c6fcc9f121185a6a610
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-04-03 15:19:35 +0530

    Update analyzeChargingCurrentRangeForADC.h

--- analyzeChargingCurrentRangeForADC.h
+++ analyzeChargingCurrentRangeForADC.h
@@ -1,4 +1,4 @@
 void interpretChargingCurrentValue(int ADC_currentValues[], size_t numSamples, int* chargingCurrentValues);
 float convertADCValueIntoCurrentValue(float conversionFactor, float scalingFactor, int minCurrentValue, int maxCurrentValue);
 void checkIfInputADCValuesAreValid(int* ADC_currentValues, size_t numSamples, int *valid_ADC_currentValues);
-extern void validateAndGetChargingCurrentRangeAndSamplesForADC(int* ADC_currentValues, int *chargingCurrentSamples, int* valid_ADC_currentValues, size_t numSamples, size_t* numValidSamples);
+extern void validateAndGetChargingCurrentRangeAndSamplesForADC(int* ADC_currentValues, int *chargingCurrentSamples, int* valid_ADC_currentValues, size_t numSamples, size_t *numValidSamples);
