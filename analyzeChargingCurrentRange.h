52e289af60a95245ae811a29b3e6e9c1a8d46897
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-04-03 15:11:21 +0530

    Update analyzeChargingCurrentRange.h

--- analyzeChargingCurrentRange.h
+++ analyzeChargingCurrentRange.h
@@ -1,5 +1,18 @@
 #include "validateAndSortRange.h"
 
+#define ADC_RESOLUTION 12
+#define MIN_CURRENT_VALUE 0
+#define MAX_CURRENT_VALUE 10
+
+//Define the Parameter Operating Structure
+typedef struct {
+  int minCurrentValue;
+  int maxCurrentValue;
+  int maxPossibleInteger;
+  int currentRange;
+  float scaleFactor;
+} ADC_OperatingStructure;
+
 void getChargingCurrentRangeAndSamples(int* samplesOfChargingCurrent, int numSamples, char* rangeAndOccurences[]);
 void buildRanges(int* sortedSamplesOfChargingCurrent, int numSamples, RangeStructure* rangeStructureArray);
 void printOutputinCsv(size_t sizeOfRangeStructureArray, RangeStructure rangeStructureArray[], char* rangeAndOccurences[]);
