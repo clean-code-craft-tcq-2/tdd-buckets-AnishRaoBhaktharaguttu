0a54de0ae47988888c2819e0b8551219237b5318
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-04-03 15:11:07 +0530

    Update analyzeChargingCurrentRangeForADC.h

--- analyzeChargingCurrentRangeForADC.h
+++ analyzeChargingCurrentRangeForADC.h
@@ -1,16 +1,3 @@
-#define ADC_RESOLUTION 12
-#define MIN_CURRENT_VALUE 0
-#define MAX_CURRENT_VALUE 10
-
-//Define the Parameter Operating Structure
-typedef struct {
-  int minCurrentValue;
-  int maxCurrentValue;
-  int maxPossibleInteger;
-  int currentRange;
-  float scaleFactor;
-} ADC_OperatingStructure;
-
 void interpretChargingCurrentValue(int ADC_currentValues[], size_t numSamples, int* chargingCurrentValues);
 float convertADCValueIntoCurrentValue(float conversionFactor, float scalingFactor, int minCurrentValue, int maxCurrentValue);
 void checkIfInputADCValuesAreValid(int* ADC_currentValues, size_t numSamples, int *valid_ADC_currentValues);
